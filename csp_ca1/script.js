b339108d1ffb64d8d6b786e2f7b790ee759e4f1a
Wandwilson Almeida da silva <wandwilson.almeida@hotmail.com>
2022-07-14 00:57:25 +0000

    update the page and add botton to the top of page

--- csp_ca1/script.js
+++ csp_ca1/script.js
@@ -3,4 +3,58 @@ const icon = document.querySelector('.icon');
 
 icon.addEventListener('click', () => {
     body.classList.toggle('dark');
-})
+});
+
+const scrollAnime = document.querySelector('[data-anima="scroll"]');
+
+const halfWindow = window.innerHeight * .05;
+
+function animeScroll(){
+    const topItem = scrollAnime.getBoundingClientRect().top;
+    
+    const visibleItem = topItem  - halfWindow < 0;
+
+    if (visibleItem){
+        scrollAnime.classList.add('show-button');
+    }else{
+        
+        scrollAnime.classList.remove('show-button');
+    };
+
+};
+
+window.addEventListener('scroll', animeScroll);
+
+
+
+const linkIntern = document.querySelector('[data-scroll="soft"] a[href^="#"]' );
+
+function ScrollTop(event){
+
+    event.preventDefault();
+
+    const href = event.currentTarget.getAttribute('href');
+
+    const anchor = document.querySelector(href);
+
+    anchor.scrollIntoView({
+        behavior: 'smooth',
+        block: 'start',
+    })
+
+    
+    
+}
+
+
+linkIntern.addEventListener('click', ScrollTop);
+
+
+
+
+
+
+
+
+
+
